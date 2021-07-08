283063e41bc00de072368d8bc23e02bcf27d2fcc
btylk <55312210+btylk@users.noreply.github.com>
2021-07-08 13:51:35 +0700

    lesson4 Validator

--- routes/data.js
+++ routes/data.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 router.get('/', function(req, res, next){
     res.render('data');
@@ -9,4 +10,15 @@ router.get('/add', function(req, res, next){
     res.render('addData');
 });
 
+router.post('/add',[
+    body("Name","Please type your name/org").not().isEmpty(),
+    body("Details","Please type your details").not().isEmpty()
+], function(req, res, next){
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors)
+    }
+    console.log(req.body.Name);
+    console.log(req.body.Details);
+});
 module.exports = router;
