81e6257d381cd289140b6c79ec9a6e165b5e7c9c
btylk <55312210+btylk@users.noreply.github.com>
2021-07-09 18:42:08 +0700

    lesson4.5 alert validation

--- routes/data.js
+++ routes/data.js
@@ -2,21 +2,25 @@ var express = require('express');
 var router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-router.get('/', function(req, res, next){
+router.get('/', function (req, res, next) {
     res.render('data');
 });
 
-router.get('/add', function(req, res, next){
+router.get('/add', function (req, res, next) {
     res.render('addData');
 });
 
-router.post('/add',[
-    body("Name","Please type your name/org").not().isEmpty(),
-    body("Details","Please type your details").not().isEmpty()
-], function(req, res, next){
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      console.log(errors)
+router.post('/add', [
+    body("Name", "Please type your name/org").not().isEmpty(),
+    body("Details", "Please type your details").not().isEmpty()
+], function (req, res, next) {
+    const result = validationResult(req);
+    var errors = result.errors;
+    if (!result.isEmpty()) {
+        res.render('addData', { errors: errors });
+        console.log(errors)
+    } else {
+        //insert to db
     }
     console.log(req.body.Name);
     console.log(req.body.Details);
